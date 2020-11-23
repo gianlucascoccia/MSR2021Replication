a6c8ec6e4bd67eaedc4d3edd6f074c43bf29bb96
Gian Luca Scoccia <loscoccia@gmail.com>
2020-11-23 18:40:01 +0100

    removed code from bodies

--- notebook/clean_stackoverflow_data.py
+++ notebook/clean_stackoverflow_data.py
@@ -8,9 +8,14 @@ import re
 
 so_questions = pd.read_csv('../data/raw/so_questions.csv')
 
+# %% Remove code from body 
+
+so_questions.Body = so_questions.Body.apply(lambda x: re.sub('\<code(.*)code>', '', x))
+
 # %% Remove HTML Tags from body 
 
 so_questions.Body = so_questions.Body.apply(lambda x: re.sub('<[^<]+?>', '', x))
+so_questions.Body = so_questions.Body.apply(lambda x: re.sub('&#xA;', '', x))
 
 # %% Store cleaned and processed data
 
