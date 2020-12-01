c5e9b1e31e7cad7550bf82b753f2c42ebace56cf
Gian Luca Scoccia <loscoccia@gmail.com>
2020-12-01 22:01:52 +0100

    added topic matrix creation script

--- notebook/parse_topics_composition.py
+++ notebook/parse_topics_composition.py
@@ -4,8 +4,9 @@ import pandas as pd
 
 # %% params
 
-IN_FILE = '../git_composition.txt'
-OUT_FILE = 'git_topic_matrix.csv'
+IN_FILE = '../data/processed/SO_T_output_Mallet/so_t_composition.txt'
+OUT_FILE = '../data/processed/SO_T_output_Mallet/git_topic_matrix.csv'
+OUT_DOCS_FOLDER = '../data/processed/SO_T_output_Mallet/topics'
 THRESHOLD = 0.5
 NUM_TOPICS = 14
 
@@ -23,7 +24,7 @@ with open(IN_FILE, 'r') as in_file:
         for i in range(1, NUM_TOPICS - 1):
             
             document.update({
-                'topic_' + str(i) : float(parts[1 + i]) > THRESHOLD
+                'topic_' + str(i) : 1 if float(parts[1 + i]) > THRESHOLD else 0
             })
 
         docs.append(document)
@@ -35,5 +36,12 @@ o = pd.DataFrame()
 o = o.append(docs)
 o = o.sort_values(by=['filename'])
 o.to_csv(OUT_FILE)
+
+
+# %% Save documents that compose each topic in file
+
+for col in o.columns[1:]:
+    t = o[ o[col] == 1 ]
+    t.to_csv(OUT_DOCS_FOLDER + '/' + col + '.csv')
 
 # %%
