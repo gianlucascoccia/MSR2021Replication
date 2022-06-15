25875cd2d476e45688d5bd3b8b0de2dfe2d68265
Gabriela Guedes <gabrielabguedes@gmail.com>
2022-06-15 00:36:05 -0300

    Generating RN topics
    Signed-off-by: Gabriela Guedes <gabrielabguedes@gmail.com>

--- notebook/unite_topics_in_one_file.py
+++ notebook/unite_topics_in_one_file.py
@@ -1,21 +1,7 @@
-import glob
 import pandas as pd
-import nltk
-STEMMING = False
-LEMMING =  True
 
-w_tokenizer = nltk.tokenize.WhitespaceTokenizer()
-lemmatizer = nltk.stem.WordNetLemmatizer()
-stemmer = nltk.stem.snowball.SnowballStemmer("english")
-
-def lemmatize_text(text):
-    return [lemmatizer.lemmatize(w) for w in w_tokenizer.tokenize(text)]
-
-def stem_text(text):
-    return [stemmer.stem(w) for w in w_tokenizer.tokenize(text)]
-
-def df_to_file(_index, _row, topic):
-    file_name = str(int(_row['filename']))
+def df_to_file(row, topic):
+    file_name = str(int(row['filename']))
 
     text_file = open("../tcc_mallet/so_data/{}.txt".format(file_name), "r")
     content = text_file.read()
@@ -26,9 +12,13 @@ def df_to_file(_index, _row, topic):
     with open(path, 'a+') as file:
       file.write(content + '\n\n')
 
+print('Uniting documents...')
+
 for i in range(1,15):
   topic = 'topic_{}'.format(i)
   print(topic)
   so = pd.read_csv('../tcc_data/processed/SO_T_output_Mallet/topics/{}.csv'.format(topic))
   for index, row in so.iterrows():
-    df_to_file(index, row,  topic)
+    df_to_file(row,  topic)
+
+print('Done!')
